82a277d2621ec3f1d8b953e0dbf37609c4b28fa7
AliAsim99 <ali.99asim@gmail.com>
2024-02-11 04:48:04 +0500

    w

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ const personSchema = new mongoose.Schema({
   
 const Person = mongoose.model('Person', personSchema)
 
-
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
 let persons=[
     { 
       "id": 1,
